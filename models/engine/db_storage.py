d726415f5886dc85e46e76d4066c37f2549f8b38
khalidhub7 <louguikhalid7@gmail.com>
2024-05-24 15:27:34 -0700

    up

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,39 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    ''' def get(self, cls, id):
+        """ retrieve one object """
+        for i in classes:
+            if i == str(cls):
+                return id.cls
+            return None
+
+    def count(self, cls=None):
+        """ count the number of objects in storage """
+        num = 0
+        for i in cls:
+            num += 1 '''
+    
+    def get(self, cls, id):
+        """
+        fetches specific object
+        :param cls: class of object as string
+        :param id: id of object as string
+        :return: found object or None
+        """
+        all_class = self.all(cls)
+
+        for obj in all_class.values():
+            if id == str(obj.id):
+                return obj
+
+        return None
+
+    def count(self, cls=None):
+        """
+        count of how many instances of a class
+        :param cls: class name
+        :return: count of instances of a class
+        """
+        return len(self.all(cls))
