0decc5e577ebf7ea3e4aa9721347214ef50ed7ca
khalidhub7 <louguikhalid7@gmail.com>
2024-05-25 08:21:54 -0700

    up

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
-""" flask functions """
+"""Initialize flask functions"""
+from flask import jsonify, make_response
 from api.v1.views import app_views
-from flask import jsonify
+from models import storage
+
+classes = {"Amenity": "amenities",
+           "City": "cities",
+           "Place": "places",
+           "Review": "reviews",
+           "State": "states",
+           "User": "users"}
 
 
 @app_views.route('/status', strict_slashes=False)
-def status():
-    """ json status """
-    return jsonify({"status": "OK"})
+def view_status():
+    """Returns a JSON"""
+    response = jsonify({"status": "OK"})
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app_views.route('/stats', strict_slashes=False)
+def storage_stats():
+    """Returns a JSON"""
+    dict = {}
+    for cls, name in classes.items():
+        dict.update({name: storage.count(cls)})
+    response = jsonify(dict)
+    response.headers["Content-Type"] = "application/json"
+    return response
