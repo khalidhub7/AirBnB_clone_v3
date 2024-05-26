feb98d5c6b9fb4b3eac886a09390da7402652d9b
khalidhub7 <louguikhalid7@gmail.com>
2024-05-26 10:16:47 -0700

    up 04

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,9 +13,14 @@ def status():
 
 @app_views.route('/stats', strict_slashes=False)
 def count_objs():
-    """Return counts of all objects by type."""
+    """Return counts of all objs"""
     classes = ["Amenity", "City", "Place", "Review", "State", "User"]
     data = {}
-    for class_name in classes:
-        data[class_name.lower() + 's'] = storage.count(class_name)
+    for i in classes:
+        data[i.lower + 's'] = storage.count(i)
+    for j in data:
+        if j == 'Amenity':
+            j = 'amenities'
+        if j == 'City':
+            j = 'cities'
     return jsonify(data)
