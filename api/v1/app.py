4db10547e3b8222f5221d576a42e8495c61a29b4
khalidhub7 <louguikhalid7@gmail.com>
2024-05-26 09:26:52 -0700

    up 03

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Its time to start the API!"""
+"""It's time to start the API!"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -10,12 +10,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
 @app.teardown_appcontext
 def close_storage(exception):
     """Close the storage on teardown."""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Help index.py to find app_views."""
-from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
