a6020d0eb09f1ea7eb834ec9b6e66a98ef31ed26
khalidhub7 <louguikhalid7@gmail.com>
2024-05-26 09:29:28 -0700

    up 03

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,10 +10,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def close_storage(exception):
     """Close the storage on teardown."""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
