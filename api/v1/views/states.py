b9c7a9b53a967d7ff8254d4b8bfca8fbc315f04a
khalidhub7 <louguikhalid7@gmail.com>
2024-05-28 08:38:18 -0700

    up

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -22,7 +22,7 @@ def get_one_obj(state_id):
     if obj is None:
         abort(404)
     if obj is not None:
-        return jsonify(obj.to_dict())
+        return jsonify(obj.to_json())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
