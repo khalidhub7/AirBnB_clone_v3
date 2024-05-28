3a4bfbe583cff207fc6c25009f8bd90001cdd1da
khalidhub7 <louguikhalid7@gmail.com>
2024-05-28 08:34:22 -0700

    up

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,7 +11,7 @@ def all_states():
     """ return list of all_states """
     list_objs = []
     for i in storage.all('State').values():
-        list_objs.append(i.to_json())
+        list_objs.append(i.to_dict())
     return jsonify(list_objs)
 
 
