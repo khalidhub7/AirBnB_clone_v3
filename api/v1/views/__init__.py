838d3fa006d6f16a7086990057ef336162f640eb
khalidhub7 <louguikhalid7@gmail.com>
2024-05-28 07:56:55 -0700

    up

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Help index.py to find app_views."""
+""" Blueprint confg for api """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
