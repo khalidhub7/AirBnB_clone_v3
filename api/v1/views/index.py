a7ed04c95c896c779b0ecc90e2fb275d6c617cea
khalidhub7 <louguikhalid7@gmail.com>
2024-08-18 18:50:23 -0700

    route stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,20 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'])
-def first_route():
+def status():
     """ returns a JSON response """
     return jsonify(
         {"status": "OK"}
     )
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ retrieve number of each object """
+    from models import storage
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    plural = ["amenities", "cities", "places", "reviews", "states", "users"]
+    data = {}
+    for cls, p in zip(classes, plural):
+        data[p] = int(storage.count(cls))
+    return jsonify(data)
